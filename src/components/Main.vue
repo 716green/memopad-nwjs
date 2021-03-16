2c19015b2e9d72fe98a64efd5533884efde07b0e
Bob Bass <bob@bbass.co>
2021-03-15 21:30:02 -0400

    added textarea and style - removed vuetify

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="hello">
     <h1>📝Memopad</h1>
+    <section class="textarea-wrap">
+      <textarea v-model="text" />
+    </section>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'Main',
-  props: ['msg']
+  data() {
+    return {
+      text: "",
+    }
+  },
+   
 }
 </script>
 
 <style scoped>
+
 h3 {
   margin: 40px 0 0;
 }
@@ -25,5 +35,18 @@ li {
 }
 a {
   color: #42b983;
+}
+.textarea-wrap {
+  width: 100%;
+}
+
+textarea {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1.2rem;
+  width: 100%;
+  height: 200px;
+  border: black solid 1px;
+  margin: auto;
+  padding: 5px;
 }
 </style>
